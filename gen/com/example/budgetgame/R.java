4a939d3b754cc7ebf94f45a12cdc20135965699a
dkchhe <dkchhe@DKLT0766.global.chgroup.net>
2014-04-03 14:33:13 +0200

    Added DBHelper and DBAdapter with methods to retrieve all posts and delete them.

--- gen/com/example/budgetgame/R.java
+++ gen/com/example/budgetgame/R.java
@@ -24,10 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int beloeb=0x7f080002;
+        public static final int dato=0x7f080001;
+        public static final int historyfragment=0x7f080003;
+        public static final int titel=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int postitem=0x7f030001;
+        public static final int postlist=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
