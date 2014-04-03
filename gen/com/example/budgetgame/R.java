c3efcdc991f6add556a7c77c66b4e0f962ac14dc
Crelde <crelde@hotmail.com>
2014-04-03 20:01:13 +0200

    Added the 4 fragments and the menu is positioned correct with some ugly buttons, the buttons are hooked up so when you press them they switch between the fragments.

--- gen/com/example/budgetgame/R.java
+++ gen/com/example/budgetgame/R.java
@@ -25,20 +25,24 @@ public final class R {
     }
     public static final class id {
         public static final int FragmentContainer=0x7f080000;
-        public static final int action_settings=0x7f080009;
-        public static final int beloeb=0x7f080007;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080004;
-        public static final int button4=0x7f080003;
-        public static final int dato=0x7f080006;
-        public static final int historyfragment=0x7f080008;
-        public static final int titel=0x7f080005;
+        public static final int action_settings=0x7f08000a;
+        public static final int beloeb=0x7f080009;
+        public static final int dato=0x7f080008;
+        public static final int goalsButton=0x7f080004;
+        public static final int homeButton=0x7f080001;
+        public static final int postsButton=0x7f080003;
+        public static final int postsfragment=0x7f080005;
+        public static final int settingsButton=0x7f080002;
+        public static final int textviewtest=0x7f080006;
+        public static final int titel=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int postitem=0x7f030001;
-        public static final int postlist=0x7f030002;
+        public static final int goals=0x7f030001;
+        public static final int overview=0x7f030002;
+        public static final int postitem=0x7f030003;
+        public static final int postlist=0x7f030004;
+        public static final int settings=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/budgetgame/MainActivity.java
+++ src/com/example/budgetgame/MainActivity.java
@@ -1,26 +1,100 @@
 package com.example.budgetgame;
 
+import com.example.budgetgame.frags.GoalFrag;
+import com.example.budgetgame.frags.OverviewFrag;
 import com.example.budgetgame.frags.PostsFrag;
+import com.example.budgetgame.frags.SettingFrag;
+
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	PostsFrag listfrag = new PostsFrag();
+	// Buttons
+	private Button homeButton;
+	private Button postsButton;
+	private Button goalsButton;
+	private Button settingsButton;
+	
+	// Fragments
+	PostsFrag postfrag = new PostsFrag();
+	OverviewFrag overfrag = new OverviewFrag();
+	GoalFrag goalfrag = new GoalFrag();
+	SettingFrag settingfrag = new SettingFrag();
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Button initializers
+		homeButton = (Button) findViewById(R.id.homeButton);
+		postsButton = (Button) findViewById(R.id.postsButton);
+		goalsButton = (Button) findViewById(R.id.goalsButton);
+		settingsButton = (Button) findViewById(R.id.settingsButton);
+	
+		// Initial adding of the overview fragment
 		FragmentManager fm = getFragmentManager();
 		FragmentTransaction ft = fm.beginTransaction();
-		
-		ft.add(R.id.FragmentContainer, listfrag);
+		ft.add(R.id.FragmentContainer, overfrag);
 		ft.commit();
+		
+
+		// Implementations of button onclicks so they change between the fragments
+		homeButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getFragmentManager();
+				FragmentTransaction ft = fm.beginTransaction();
+				ft.replace(R.id.FragmentContainer, overfrag);
+				ft.commit();;	
+			}
+		});
+		
+		postsButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getFragmentManager();
+				FragmentTransaction ft = fm.beginTransaction();
+				ft.replace(R.id.FragmentContainer, postfrag);
+				ft.commit();;	
+			}
+		});
+		
+		goalsButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getFragmentManager();
+				FragmentTransaction ft = fm.beginTransaction();
+				ft.replace(R.id.FragmentContainer, goalfrag);
+				ft.commit();;	
+			}
+		});
+		
+		settingsButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getFragmentManager();
+				FragmentTransaction ft = fm.beginTransaction();
+				ft.replace(R.id.FragmentContainer, settingfrag);
+				ft.commit();;	
+			}
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
