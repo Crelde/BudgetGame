b90d053986d6e21cd5cb420e4cb9462a9f0bc69e
kekwin <kiw_killer@hotmail.com>
2014-04-06 17:40:03 +0200

    Implemented OverviewFrag, for now with a static graph and static values.

--- gen/com/example/budgetgame/R.java
+++ gen/com/example/budgetgame/R.java
@@ -21,27 +21,42 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int graf=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int FragmentContainer=0x7f080000;
-        public static final int action_settings=0x7f080011;
-        public static final int beloeb=0x7f080010;
+        public static final int action_settings=0x7f08001f;
+        public static final int avaibleValue=0x7f080015;
+        public static final int aviableText=0x7f080010;
+        public static final int beloeb=0x7f08001d;
         public static final int button1=0x7f08000b;
+        public static final int comingText=0x7f080012;
+        public static final int comingValue=0x7f080017;
+        public static final int currText=0x7f08000f;
+        public static final int currValue=0x7f080014;
         public static final int currentStatus=0x7f080009;
         public static final int currentStatusE=0x7f08000a;
-        public static final int dato=0x7f08000f;
+        public static final int dato=0x7f08001c;
         public static final int goalAmount=0x7f080007;
         public static final int goalAmountE=0x7f080008;
         public static final int goalName=0x7f080005;
         public static final int goalNameE=0x7f080006;
         public static final int goalsButton=0x7f080004;
         public static final int homeButton=0x7f080001;
+        public static final int imageView1=0x7f08000d;
+        public static final int payDayText=0x7f080013;
+        public static final int payDayValue=0x7f080018;
         public static final int postsButton=0x7f080003;
         public static final int postsfragment=0x7f08000c;
+        public static final int prevText=0x7f080011;
+        public static final int prevValue=0x7f080016;
+        public static final int scrollView=0x7f08000e;
         public static final int settingsButton=0x7f080002;
-        public static final int textviewtest=0x7f08000d;
-        public static final int titel=0x7f08000e;
+        public static final int syncedText=0x7f080019;
+        public static final int syncedValue=0x7f08001a;
+        public static final int textviewtest=0x7f08001e;
+        public static final int titel=0x7f08001b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -58,8 +73,23 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int avaible=0x7f050007;
+        public static final int avaibleVal=0x7f050008;
         public static final int crelde=0x7f050003;
+        public static final int currSaldo=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int imageDesc=0x7f050004;
+        public static final int payDayIn=0x7f05000d;
+        public static final int payDayVal=0x7f05000e;
+        public static final int perDayComing=0x7f05000b;
+        public static final int perDayComingVal=0x7f05000c;
+        public static final int perDayPrev=0x7f050009;
+        public static final int perDayPrevVal=0x7f05000a;
+        public static final int saldoVal=0x7f050006;
+        public static final int savings=0x7f05000f;
+        public static final int savingsVal=0x7f050010;
+        public static final int synced=0x7f050011;
+        public static final int syncedVal=0x7f050012;
     }
     public static final class style {
         /** 
